cd42c33ac3c28d5c7d97c401779ed2aa358c68f7
Alexander Nikolaev <demosfen.spb@gmail.com>
2023-02-20 10:38:26 +0300

    Create KpClassificationByDailySum.py

---
+++ src/Python/tools/KpClassificator/KpClassificationByDailySum.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This simple script classifies daily activity using Kp thresholds.
+They are:
+    No activity: Sum(Kp) <= 15
+    Weak-to-moderate activity: 15 < Sum(Kp) < 24
+    Disturbed conditions: Kp >= 24
+
+--- HOW TO USE ---
+
+1. ...
+
+Created on Mon Feb 20 10:32:14 2023
+
+@author: Alexander Nikolaev
+"""
