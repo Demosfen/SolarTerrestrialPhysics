b5754b805124f99ff5cafcc67fc52bab93e4cb1a
Alexander Nikolaev <demosfen.spb@gmail.com>
2023-02-20 16:39:41 +0300

    Update HelperRoutines.py

--- src/Python/tools/KpClassificator/HelperRoutines.py
+++ src/Python/tools/KpClassificator/HelperRoutines.py
@@ -1,12 +1,10 @@
 """
-
+Simple data file reader.
 """
 
 
-# --- Read ASCII file ---
 def io(path):
     """
-
     :rtype: Data rows in string format
     """
     file = open(path, 'r')
@@ -15,9 +13,17 @@ def io(path):
     return data
 
 
+"""
+The standard Kp values look like 0, 0+, 1-, 1, 1+, 2-,
+but are stored as Kp = 0, 3, 7, 10, 13, 17, ... in the OMNI data set.
+OMNI have mapped 0+ to 3, 1- to 7, 1 to 10, 1+ to 13, 2- to 17, etc.
+For example Kp = 7+ is coded as 73; Kp = 7- is coded as 77; Kp = 7 as 70
+"""
+
+
 def kp_classification(value):
     match value:
-        case "3":
+        case "0" | "3":
             return 0
         case "7" | "10" | "13":
             return 1
@@ -37,3 +43,39 @@ def kp_classification(value):
             return 8
         case "87" | "90" | "93":
             return 9
+
+
+# --- Epoch and Substorm onset compare ---
+
+def epoch_index(cycle, time):
+    k = -1
+
+    for i in range(len(cycle)):
+        if int(time) in range(int(cycle[i][0]), int(cycle[i][1]), 50):
+            k = i
+
+    return k
+
+
+# --- Season search ---
+
+def season_index(month):
+    if month in range(3, 6) or month in range(9, 12):
+        return 2  # off-season
+    elif month in range(6, 9):
+        return 1  # Summer
+    else:
+        return 0  # Winter
+
+
+# --- Print list ---
+
+def print_list(path, name, outputHeader, toPrint):
+    file = open(path + name + '.lst', 'w')
+    file.write(outputHeader)
+    file.write('\n')
+
+    for line in toPrint:
+        file.write(line)
+        file.write('\n')
+    file.close()
